2e03abc7c90f3277e11efaf7734dbb73ddb3bb9d
Kanak Sao <145253539+Kanaksao18@users.noreply.github.com>
2025-07-17 01:02:37 +0530

    Fixed Length Problem

--- Arrays/Sliding Window/MaxSubArraySumWithSizeOfK.java
+++ Arrays/Sliding Window/MaxSubArraySumWithSizeOfK.java
@@ -3,7 +3,7 @@ public int maxSumSubarray(int[] arr, int k) {
     int windowSum = 0, maxSum = 0;
     for (int i = 0; i < k; i++) {
         windowSum += arr[i];
-    }
+    } 
     maxSum = windowSum;
 
     for (int i = k; i < arr.length; i++) {
